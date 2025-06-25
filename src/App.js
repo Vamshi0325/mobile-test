bbc04515a1c2ecd9d9bd30bfa2e01b27f263e520
Vamshi <vamshivade2000@gmail.com>
2025-06-25 15:43:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
-// import { useTelegram } from "./hooks/useTelegram";
-// import UnsupportedPlatform from "./components/UnsupportedPlatform";
-// import "./App.css";
+import { useTelegram } from "./hooks/useTelegram";
+import UnsupportedPlatform from "./components/UnsupportedPlatform";
+import "./App.css";
 
-// function App() {
-//   const { isValidTelegram } = useTelegram();
+function App() {
+  const { isValidTelegram } = useTelegram();
 
-//   if (!isValidTelegram) {
-//     console.warn("🚫 Unsupported Platform Detected");
-//     return <UnsupportedPlatform />;
-//   }
+  if (!isValidTelegram) {
+    console.warn("🚫 Unsupported Platform Detected");
+    return <UnsupportedPlatform />;
+  }
 
-//   const tgUser = window.Telegram?.WebApp?.initDataUnsafe?.user;
+  const tgUser = window.Telegram?.WebApp?.initDataUnsafe?.user;
 
-//   return (
-//     <div className="telegram-app">
-//       <h1>Welcome to Our Bot! 🎉</h1>
-//       <p>You're using the official Telegram app.</p>
-
-//       {tgUser && <p>Hello, {tgUser.first_name}!</p>}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React from "react";
-
-const App = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        color: "black",
-        fontSize: "40px",
-      }}
-    >
-      App
+    <div className="telegram-app">
+      <h1>Welcome to Our Bot! 🎉</h1>
+      <p>You're using the official Telegram app.</p>
+
+      {tgUser && <p>Hello, {tgUser.first_name}!</p>}
     </div>
   );
-};
+}
 
 export default App;
